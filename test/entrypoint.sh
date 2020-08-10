903107aea9ffa854b3117b88e7a1a8b0a8c1d90f
Gazorby <gazorby@pm.me>
2020-08-10 20:01:36 +0200

    ✅ tests: configure chezmoi repo

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-/bin/chezmoi init https://github.com/Gazorby/dotfiles.git
+/bin/chezmoi init $GIT_REPO
 sed -i '/^.*starship init fish.*$/d' ~/.local/share/chezmoi/private_dot_config/private_fish/config.fish.tmpl
 
 exec "$@"
