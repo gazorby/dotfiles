6cd621334734fc3b04e6bdc76d17f0f5a07c8f3c
Gazorby <gazorby@pm.me>
2021-03-25 02:03:48 +0100

    ♻️ refactor(fish): update fif function

--- private_dot_config/private_fish/functions/fif.fish
+++ private_dot_config/private_fish/functions/fif.fish
@@ -3,5 +3,5 @@ function fif -d "Search file content. usage : fif <string>"
         echo "Need a string to search for!"
         return 1
     end
-    rg --hidden --files-with-matches --no-messages -g "!{.git,node_modules}/*" "$argv[1]" | fzf --preview "bat --style=numbers --color=always {} | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$argv[1]' || rg --ignore-case --pretty --context 10 '$argv[1]' {}"
+    rg --hidden --files-with-matches --no-messages -g "!{.git,node_modules}/*" "$argv[1]" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$argv[1]' || rg --ignore-case --pretty --context 10 '$argv[1]' {}"
 end
