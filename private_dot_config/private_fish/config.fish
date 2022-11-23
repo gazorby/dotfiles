daa82033923026ae5a8f75ab9f57af71cae9b8f9
gazorby <gazorby@pm.me>
2022-11-23 12:44:54 +0100

    ♻️ refactor: update config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -17,6 +17,7 @@ set -ag fish_user_paths $HOME/.cargo/bin
 set -ag fish_user_paths $HOME/.dotnet
 set -ag fish_user_paths $HOME/.yarn/bin
 set -ag fish_user_paths $HOME/.poetry/bin
+set -ag fish_user_paths $HOME/.krew/bin
 
 set -q fisher_path; or set -Ux fisher_path "$HOME/.config/fish"
 
@@ -44,19 +45,27 @@ end
 
 # fzf
 if type -q fzf
-    # margin, cycle scrolling, layout, height and color
-    # set -gx FZF_DEFAULT_OPTS "--cycle --reverse --height 60% --color 'fg:#bbccdd,fg+:#ddeeff,bg:#1B1C1D,preview-bg:#1B1C1D,border:#778899'"
-
+    set -Ux FZF_DEFAULT_OPTS "
+        --layout=reverse
+        --height=90%
+        --prompt='~ ' --pointer='▶' --marker='✓'
+        --multi
+        --color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
+        --bind='ctrl-y:execute-silent(echo {+} | xclip)'
+        --bind='ctrl-a:select-all'
+        --bind='?:toggle-preview'
+        --bind='ctrl-o:execute(fzf_file_open {+} &> /dev/tty)' --bind='ctrl-v:execute(code {+})'
+    "
     # Use mcfly for searching history, fallback to fzf.fish
     # both are ctrl-r binded
     if type -q mcfly
-        fzf_configure_bindings --history
+        fzf_configure_bindings --directory=\cf --processes=\cp
         mcfly init fish | source
         set -gx MCFLY_FUZZY 2
         set -gx MCFLY_RESULTS 20
         set -gx MCFLY_INTERFACE_VIEW BOTTOM
     else
-        fzf_configure_bindings
+        fzf_configure_bindings --history=\cr --directory=\cf --processes=\cp
     end
 
     # Use exa to list files (with colors) if present
@@ -64,18 +73,29 @@ if type -q fzf
         set -gx fzf_preview_dir_cmd exa --all --color=always
     end
 
+    set -gx fzf_fd_opts --follow
+    # Bind ctrl+h to reload with hidden files
+    set -a fzf_dir_opts --bind='ctrl-h:reload(fd --type file --color=always --hidden --follow --exclude .git)'
+    # Bind ctrl+x to reload with executable files
+    set -a fzf_dir_opts --bind='ctrl-x:reload(fd --type executable --color=always --hidden --follow --exclude .git)'
+    # Bind ctrl+d to reload with directories only
+    set -a fzf_dir_opts --bind='ctrl-d:reload(fd --type directory --color=always --hidden --follow --exclude .git)'
+    # Bind ctrl+f to reload with the default search options
+    set -a fzf_dir_opts --bind='ctrl-f:reload(fd --type file --color=always --follow)'
+    # Bind ctrl+o to open the current item
+    set -a fzf_dir_opts --bind="ctrl-o:execute(nvim {} &> /dev/tty)"
+
+    set -gx fzf_preview_file_cmd fzf_file_preview
+
     # Use bat to preview files if present
     if not type -q bat
         set -gx fzf_preview_file_cmd cat
     end
-
+    # find -L . \( -path ./.git -prune -path ./node_modules -prune \) -o -print -type f 2> /dev/null
     # Use delta to show git diff when searching through git log
     if type -q delta
         set -gx fzf_git_log_opts --preview='git show {1} | delta'
     end
-
-    # Use ctrl+o when searching paths to open the current item
-    set --export fzf_dir_opts --bind "ctrl-o:execute(vim {} &> /dev/tty)"
 
     # Forgit plugin
     set -U forgit_log glo:
@@ -144,3 +164,19 @@ if type -q direnv
 end
 
 set -gx GPG_TTY (tty)
+
+# fifc
+fifc \
+    -r '^(pacman|paru)(\\h*\\-S)?\\h+' \
+    -s 'pacman --color=always -Ss "$fifc_token" | string match -r \'^[^\\h+].*\'' \
+    -e '.*/(.*?)\\h.*' \
+    -f "--query ''" \
+    -p 'pacman -Si "$fifc_extracted"'
+
+fifc \
+    -r '.*\*{2}.*' \
+    -s 'rg --hidden -l --no-messages (string match -r -g \'.*\*{2}(.*)\' "$fifc_commandline")' \
+    -p 'batgrep --color --paging=never (string match -r -g \'.*\*{2}(.*)\' "$fifc_commandline") "$fifc_candidate"' \
+    -f "--query ''" \
+    -o 'batgrep --color (string match -r -g \'.*\*{2}(.*)\' "$fifc_commandline") "$fifc_candidate" | less -R' \
+    -O 1
