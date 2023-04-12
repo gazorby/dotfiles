31c643350fc93acf900d9d8121bf95c362358b2a
gazorby <gazorby@pm.me>
2023-04-12 15:54:19 +0200

    : update fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -188,7 +188,3 @@ if type -q scw
     # Scaleway CLI autocomplete initialization.
     eval (scw autocomplete script shell=fish)
 end
-
-# https://github.com/jhillyerd/plugin-git/issues/90
-set -e __git_plugin_initialized
-__git.init
