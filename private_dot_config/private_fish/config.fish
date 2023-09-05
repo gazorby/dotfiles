6893085b0a952216ecb94c8d12919b4b0785c5fd
gazorby <gazorby@pm.me>
2023-09-05 17:33:44 +0200

    perf(fish): remove type -q calls in config.fish, remove unused plugins
    BREAKING CHANGE:

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -25,85 +25,66 @@ set -gx CARGO_INSTALL_ROOT ~/.cargo
 # Colors
 set -x LS_COLORS (vivid generate molokai)
 
-
 # Go
-if type -q go
-    set -q GOPATH; or set -Ux GOPATH "$HOME/go"
-    set -q GOBIN; or set -Ux GOBIN "$GOPATH/bin"
-    set -q GO111MODULE; or set -Ux GO111MODULE on
-    set -ag fish_user_paths "$GOBIN"
-end
+set -q GOPATH; or set -Ux GOPATH "$HOME/go"
+set -q GOBIN; or set -Ux GOBIN "$GOPATH/bin"
+set -q GO111MODULE; or set -Ux GO111MODULE on
+set -ag fish_user_paths "$GOBIN"
+# end
 
 # Colorize manpages using bat
-if type -q bat
-    abbr b bat
-    set -q MANPAGER; or set -Ux MANPAGER 'sh -c "col -bx | bat --language=man --style=grid --color=always --decorations=always"'
-    set -q MANROFFOPT; or set -Ux MANROFFOPT -c
-end
+set -q MANPAGER; or set -Ux MANPAGER 'sh -c "col -bx | bat --language=man --style=grid --color=always --decorations=always"'
+set -q MANROFFOPT; or set -Ux MANROFFOPT -c
 
-if type -q nvim
-    abbr nv nvim
-end
-
+###################################
 # fzf
-if type -q fzf
-    set -Ux FZF_DEFAULT_OPTS "
-        --layout=reverse
-        --height=90%
-        --prompt='~ ' --pointer='▶' --marker='✓'
-        --multi
-        --color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
-        --bind='ctrl-y:execute-silent(echo {+} | xclip)'
-        --bind='ctrl-a:select-all'
-        --bind='?:toggle-preview'
-        --bind='ctrl-o:execute(fzf_file_open {+} &> /dev/tty)' --bind='ctrl-v:execute(code {+})'
-    "
+###################################
+set -gx FZF_DEFAULT_OPTS "
+    --layout=reverse
+    --height=90%
+    --prompt='~ ' --pointer='▶' --marker='✓'
+    --multi
+    --color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
+    --bind='ctrl-y:execute-silent(echo {+} | xclip)'
+    --bind='ctrl-a:select-all'
+    --bind='?:toggle-preview'
+    --bind='ctrl-o:execute(nvim {+} &> /dev/tty)'
+    --bind='ctrl-v:execute(code {+})'
+"
 
-    if type -q fzf_configure_bindings
-        fzf_configure_bindings --directory=\cf --processes=\cp --git_log=\cg --history=\cr
-    end
+# if type -q fzf_configure_bindings
+fzf_configure_bindings --directory=\cf --processes=\cp --git_log=\cg --history=\cr
 
-    # Use exa to list files (with colors) if present
-    if type -q exa
-        set -gx fzf_preview_dir_cmd exa --all --color=always
-    end
+# Use exa to list files (with colors) if present
+set -gx fzf_preview_dir_cmd exa --all --color=always
 
-    set -gx fzf_fd_opts --follow
-    # Bind ctrl+h to reload with hidden files
-    set -a fzf_dir_opts --bind='ctrl-h:reload(fd --type file --color=always --hidden --follow --exclude .git)'
-    # Bind ctrl+x to reload with executable files
-    set -a fzf_dir_opts --bind='ctrl-x:reload(fd --type executable --color=always --hidden --follow --exclude .git)'
-    # Bind ctrl+d to reload with directories only
-    set -a fzf_dir_opts --bind='ctrl-d:reload(fd --type directory --color=always --hidden --follow --exclude .git)'
-    # Bind ctrl+f to reload with the default search options
-    set -a fzf_dir_opts --bind='ctrl-f:reload(fd --type file --color=always --follow)'
-    # Bind ctrl+o to open the current item
-    set -a fzf_dir_opts --bind="ctrl-o:execute(nvim {} &> /dev/tty)"
+set -gx fzf_fd_opts --follow
+# Bind ctrl+h to reload with hidden files
+set -a fzf_dir_opts --bind='ctrl-h:reload(fd --type file --color=always --hidden --follow --exclude .git)'
+# Bind ctrl+x to reload with executable files
+set -a fzf_dir_opts --bind='ctrl-x:reload(fd --type executable --color=always --hidden --follow --exclude .git)'
+# Bind ctrl+d to reload with directories only
+set -a fzf_dir_opts --bind='ctrl-d:reload(fd --type directory --color=always --hidden --follow --exclude .git)'
+# Bind ctrl+f to reload with the default search options
+set -a fzf_dir_opts --bind='ctrl-f:reload(fd --type file --color=always --follow)'
+# Bind ctrl+o to open the current item
+set -a fzf_dir_opts --bind="ctrl-o:execute(nvim {} &> /dev/tty)"
 
-    set -gx fzf_preview_file_cmd fzf_file_preview
+# find -L . \( -path ./.git -prune -path ./node_modules -prune \) -o -print -type f 2> /dev/null
+# Use delta to show git diff when searching through git log
+set -gx fzf_git_log_opts --preview='git show {1} | delta'
 
-    # Use bat to preview files if present
-    if not type -q bat
-        set -gx fzf_preview_file_cmd cat
-    end
-    # find -L . \( -path ./.git -prune -path ./node_modules -prune \) -o -print -type f 2> /dev/null
-    # Use delta to show git diff when searching through git log
-    if type -q delta
-        set -gx fzf_git_log_opts --preview='git show {1} | delta'
-    end
-
-    # Forgit plugin
-    set -U forgit_log glo:
-    set -U forgit_diff gd:
-    set -U forgit_add ga:
-    set -U forgit_reset_head grh:
-    set -U forgit_ignore gif
-    set -U forgit_restore gcf:
-    set -U forgit_clean gclean:
-    set -U forgit_stash_show gss:
-    set -U forgit_cherry_pick gcp:
-    set -U forgit_rebase grb:
-end
+# Forgit plugin
+set -U forgit_log glo:
+set -U forgit_diff gd:
+set -U forgit_add ga:
+set -U forgit_reset_head grh:
+set -U forgit_ignore gif
+set -U forgit_restore gcf:
+set -U forgit_clean gclean:
+set -U forgit_stash_show gss:
+set -U forgit_cherry_pick gcp:
+set -U forgit_rebase grb:
 
 ###################################
 # Auto add ssh keys at login
@@ -130,7 +111,6 @@ if status --is-login
 
 end
 
-
 ###################################
 # Keybindings
 ###################################
@@ -138,47 +118,36 @@ end
 bind \cH backward-kill-path-component
 
 ###################################
-# Aliases
+# Aliases/Abbreviations
 ###################################
 
 alias cl clear
 alias cf fzf-bcd-widget
 
+abbr b bat
+abbr nv nvim
+
 ###################################
 # Sources
 ###################################
 
-if type -q starship
-    eval (starship init fish)
-end
+eval (starship init fish)
 
 source /opt/asdf-vm/asdf.fish
 
-if type -q direnv
-    direnv hook fish | source
-end
-
 set -gx GPG_TTY (tty)
 
-# fifc
-if type -q fifc
-    fifc \
-        -r '^(pacman|paru)(\\h*\\-S)?\\h+' \
-        -s 'pacman --color=always -Ss "$fifc_token" | string match -r \'^[^\\h+].*\'' \
-        -e '.*/(.*?)\\h.*' \
-        -f "--query ''" \
-        -p 'pacman -Si "$fifc_extracted"'
+fifc \
+    -r '^(pacman|paru)(\\h*\\-S)?\\h+' \
+    -s 'pacman --color=always -Ss "$fifc_token" | string match -r \'^[^\\h+].*\'' \
+    -e '.*/(.*?)\\h.*' \
+    -f "--query ''" \
+    -p 'pacman -Si "$fifc_extracted"'
 
-    fifc \
-        -r '.*\*{2}.*' \
-        -s 'rg --hidden -l --no-messages (string match -r -g \'.*\*{2}(.*)\' "$fifc_commandline")' \
-        -p 'batgrep --color --paging=never (string match -r -g \'.*\*{2}(.*)\' "$fifc_commandline") "$fifc_candidate"' \
-        -f "--query ''" \
-        -o 'batgrep --color (string match -r -g \'.*\*{2}(.*)\' "$fifc_commandline") "$fifc_candidate" | less -R' \
-        -O 1
-end
-
-if type -q scw
-    # Scaleway CLI autocomplete initialization.
-    eval (scw autocomplete script shell=fish)
-end
+fifc \
+    -r '.*\*{2}.*' \
+    -s 'rg --hidden -l --no-messages (string match -r -g \'.*\*{2}(.*)\' "$fifc_commandline")' \
+    -p 'batgrep --color --paging=never (string match -r -g \'.*\*{2}(.*)\' "$fifc_commandline") "$fifc_candidate"' \
+    -f "--query ''" \
+    -o 'batgrep --color (string match -r -g \'.*\*{2}(.*)\' "$fifc_commandline") "$fifc_candidate" | less -R' \
+    -O 1
