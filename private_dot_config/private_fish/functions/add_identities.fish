2176f2646a05f70c14fb31af98d77f09ebdb0cb2
gazorby <gazorby@pm.me>
2023-09-06 12:20:03 +0200

    refactor(add_identities): make string match quiet

--- private_dot_config/private_fish/functions/add_identities.fish
+++ private_dot_config/private_fish/functions/add_identities.fish
@@ -1,7 +1,7 @@
 function add_identities -d "Add ssh identities"
     set -l existing_keys (ssh-add -l)
     for file in (grep -slR "PRIVATE" $HOME/.ssh/)
-        if string match (string join '' '*' (ssh-keygen -lf "$file") '*') "$existing_keys"
+        if not string match -q (string join '' '*' (ssh-keygen -lf "$file") '*') "$existing_keys"
             ssh-add "$file"
         end
     end
